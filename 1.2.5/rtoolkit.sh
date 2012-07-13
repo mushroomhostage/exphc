53f859867bc1481e1436b4435e03621dbd51e617
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-07-12 18:23:29 -0700

    Attempt at fixing Server crash: Remote Toolkit: java.lang.OutOfMemoryError: GC overhead limit exceeded https://github.com/mushroomhostage/exphc/issues/63

--- 1.2.5/rtoolkit.sh
+++ 1.2.5/rtoolkit.sh
@@ -5,4 +5,5 @@ PASS=pass
 
 ##YOU SHOULD NOT NEED TO EDIT ANYTHING BELOW THIS LINE##
 DIRECTORY=$(cd "`dirname "$0"`" && pwd)
-(cd "$DIRECTORY"; java -Xms30M -XX:MaxPermSize=40M -jar Minecraft_RKit.jar ${USER}:${PASS})
+(cd "$DIRECTORY"; java -Xms40M -jar Minecraft_RKit.jar ${USER}:${PASS})
+
